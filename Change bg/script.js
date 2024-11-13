2dcb522fbc072ccc7be61f5adf20e9c7dc020827
Hossein <hosseinyasinzadeh2002@gmail.com>
2024-11-13 17:41:12 +0330

    add change bg train

---
+++ Change bg/script.js
@@ -0,0 +1,9 @@
+function getRandomColor() {
+  const red = Math.floor(Math.random() * 256);
+  const green = Math.floor(Math.random() * 256);
+  const blue = Math.floor(Math.random() * 256);
+  return `rgb(${red}, ${green}, ${blue})`;
+}
+setInterval(() => {
+  document.body.style.backgroundColor = getRandomColor();
+}, 2000);
